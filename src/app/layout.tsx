402df34c235b6ca478fb54a13028754d7cdd3569
vincecoscia <xvincentcosciax@gmail.com>
2025-08-22 18:19:47 -0400

    Update favicon and enhance RootLayout with mobile web app title
    - Replaced the favicon.ico file for improved branding.
    - Added a meta tag for the mobile web app title in the RootLayout component to enhance user experience on Apple devices.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="FFSN" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
